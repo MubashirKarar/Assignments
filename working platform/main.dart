93e3deb37640a3fe4e3f08b5014f4a186104a17c
Mubashir Karar <mubashirkarar73@gmail.com>
2024-09-04 10:52:01 -0700

    only can be read

---
+++ working platform/main.dart
@@ -0,0 +1,67 @@
+void main() {
+  List namelist = ['Ali', 'Bilal', 'AbdulRafay', 'Faizan'];
+  print(namelist);
+  List dayslist = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+  print(dayslist.elementAt(0));
+  Map studentinfo = {
+    'Name': 'Mahad',
+    'Class': 8,
+    'Rollno': 5,
+    'Grade': 'A',
+    'Persentage': 85
+  };
+  print(studentinfo);
+  List number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  print(number.first);
+  print(number.last);
+  List<int> numbers = [10, 5, 100, 3, 47];
+
+  // Check if the list is not empty
+  if (numbers.isEmpty) {
+    print('The list is empty.');
+    return;
+  }
+
+  // Use the reduce method to find the maximum value
+  int maxNumber = numbers.reduce((a, b) => a > b ? a : b);
+
+  print('The maximum value is: $maxNumber');
+  List<String> originallist = ['apple', 'mango', 'banana'];
+  List<String> reversedList = originallist.reversed.toList();
+  print('originallist;$originallist');
+  print('reversedList:$reversedList');
+  filterPositiveNumbers(List<int> numbers) {
+    // Use the where() method to filter out negative numbers
+    List<int> positiveNumbers = numbers.where((number) => number >= 0).toList();
+
+    // Print the new list containing only positive numbers
+    print('Positive numbers: $positiveNumbers');
+  }
+
+  // Example list of integers
+  List<int> originalList = [10, -5, 7, -3, 12, -8];
+
+  // Call the function with the original list
+  filterPositiveNumbers(originalList);
+  List<String> usersEligibility = [
+    'John',
+    'Alice',
+    'eligible',
+    'Mike',
+    'Sarah',
+    'Tom'
+  ];
+
+  // Remove all values that are not 'eligible'
+  usersEligibility.removeWhere((user) => user != 'eligible');
+
+  print('Filtered list: $usersEligibility');
+}

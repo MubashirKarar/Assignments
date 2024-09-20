9c0d308061ac28eab777e73f56b1596552cc9372
Mubashir Karar <mubashirkarar73@gmail.com>
2024-09-20 10:58:53 -0700

    please don not read

--- main.dart
+++ main.dart
@@ -1,48 +1,22 @@
 void main() {
-  // Create the map with the given key-value pairs
-  Map<String, dynamic> car = {
-    'brand': 'Toyota',
-    'color': 'Red',
-    'isSedan': true,
-  };
+  int temperature = 30;
 
-  // Check if the car is a sedan and red in color
-  if (car['isSedan'] == true && car['color'] == 'Red') {
-    print('Match');
+  if (temperature > 30) {
+    print("It's hot outside.");
+  } else if (temperature < 20) {
+    print("It's cold outside.");
   } else {
-    print('No match');
+    print("The weather is mild.");
   }
-  Map<String, dynamic> user = {
-    'name': 'Alice',
-    'isAdmin': true,
-    'isActive': true,
-  };
+  var count = 0;
+  do {
+    print('Count: $count');
+    count++;
+  } while (count < 5);
+  var fruits = Map<String, int>();
+  fruits['apple'] = 1;
+  fruits['banana'] = 2;
+  fruits['cherry'] = 3;
 
-  // Check if the user is both an admin and active
-  if (user['isAdmin'] == true && user['isActive'] == true) {
-    print('Active admin');
-  } else {
-    print('Not an active admin');
-  }
-  int number = 5;
-
-  // Print the multiplication table
-  for (int i = 1; i <= 10; i++) {
-    print('$number x $i = ${number * i}');
-  }
-  // Define the list of numbers
-  List<int> numbers = [3, 9, 1, 6, 4, 2, 8, 5, 7];
-
-  // Initialize the largest element with the first element of the list
-  int largest = numbers[0];
-
-  // Iterate through the list to find the largest element
-  for (int i = 1; i < numbers.length; i++) {
-    if (numbers[i] > largest) {
-      largest = numbers[i];
-    }
-  }
-
-  // Print the largest element
-  print('Largest element: $largest');
+  print(fruits); // Output: {apple: 1, banana: 2, cherry: 3}
 }
